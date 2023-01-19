27f3863225cb2994b71ef6351692d4e5b73d996b
developer <andrewkrupenko338@gmail.com>
2023-01-19 11:18:42 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 }
 
